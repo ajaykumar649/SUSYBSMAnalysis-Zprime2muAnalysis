5b683c776e7613e3073e1280c7116a517634646e
Kurt <randomk91@gmail.com>
2015-02-11 18:14:49 +0100

    First iter

---
+++ test/HistosMiniAOD.py
@@ -0,0 +1,7 @@
+import FWCore.ParameterSet.Config as cms
+
+from SUSYBSMAnalysis.Zprime2muAnalysis.Zprime2muAnalysis_cfg import process
+
+process.load('SUSYBSMAnalysis.Zprime2muAnalysis.HistosFromPAT_cfi')
+
+process.p = cms.Path(process.Zprime2muAnalysisSequence * process.HistosFromPAT)
