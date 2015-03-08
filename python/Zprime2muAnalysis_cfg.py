8e892357f6f79dc2460ebda6de84163951409c5e
Kurt <randomk91@gmail.com>
2015-03-08 05:56:38 +0100

    Replace p4

--- python/Zprime2muAnalysis_cfg.py
+++ python/Zprime2muAnalysis_cfg.py
@@ -2,7 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('Zprime2muAnalysis')
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
-process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('file:///afs/cern.ch/user/k/klarson/private/miniWork/CMSSW_7_2_0/src/SUSYBSMAnalysis/Zprime2muAnalysis/MyOutputFile.root', '/store/relval/CMSSW_7_2_0/RelValZMM_13/MINIAODSIM/PU25ns_PHYS14_25_V1_Phys14-v2/00000/EA3D8F7C-A059-E411-8858-0025905A6136.root'))
+process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('/store/mc/Phys14DR/ZprimeToMuMu_M-5000_Tune4C_13TeV-pythia8/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/00000/788396C0-9D6F-E411-97DF-002590494E34.root',
+	'/store/mc/Phys14DR/ZprimeToMuMu_M-5000_Tune4C_13TeV-pythia8/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/00000/3023C1A6-D56F-E411-B210-002590AC4C08.root',
+	'/store/mc/Phys14DR/ZprimeToMuMu_M-5000_Tune4C_13TeV-pythia8/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/00000/90575C8B-D56F-E411-A39B-0025904B1420.root',
+	'/store/mc/Phys14DR/ZprimeToMuMu_M-5000_Tune4C_13TeV-pythia8/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/00000/FCEF82CA-9D6F-E411-A2FC-002481E0D5CE.root',
+	'/store/mc/Phys14DR/ZprimeToMuMu_M-5000_Tune4C_13TeV-pythia8/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/10000/44B8EEE7-9A6F-E411-8858-002590DB0640.root',
+	'/store/mc/Phys14DR/ZprimeToMuMu_M-5000_Tune4C_13TeV-pythia8/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/10000/F03F87D0-9A6F-E411-8BC4-00266CFFA5E0.root'))
+#file:///afs/cern.ch/user/k/klarson/private/miniWork/CMSSW_7_2_0/src/SUSYBSMAnalysis/Zprime2muAnalysis/MyOutputFile.root', '/store/relval/CMSSW_7_2_0/RelValZMM_13/MINIAODSIM/PU25ns_PHYS14_25_V1_Phys14-v2/00000/EA3D8F7C-A059-E411-8858-0025905A6136.root'))
 #'))#/store/relval/CMSSW_7_2_0/RelValZMM_13/MINIAODSIM/PU25ns_PHYS14_25_V1_Phys14-v2/00000/0A3BFB7A-A059-E411-A9C1-0025905A48D6.root'))
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 
